b545f6e920fe60e8faf72861e89ed5d247f238b6
halithacbekkur <mfadlatjamyh@gmail.com>
2025-10-18 21:19:18 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,13 @@
+String ad = "Halid";
+        String soyad = "Hacbekkur";
+        String ogrNo = "250541608";
+        String bolum = "Yazılım Mühendisliği";
+
+        System.out.println("*****************************");
+        System.out.println("*        İSİM KARTI        *");
+        System.out.println("*****************************");
+        System.out.println("* Ad        : " + ad);
+        System.out.println("* Soyad     : " + soyad);
+        System.out.println("* Öğrenci No: " + ogrNo);
+        System.out.println("* Bölüm     : " + bolum);
+        System.out.println("*****************************");
