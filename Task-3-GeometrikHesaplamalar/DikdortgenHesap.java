32b0604b724d2e0de28cae311c7b67dabeb30a4d
halithacbekkur <mfadlatjamyh@gmail.com>
2025-10-18 21:07:59 +0300

    Add rectangle area and perimeter calculations

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,12 @@
+double en = 4.5;
+                double boy = 7.9;
+
+                // Alan ve çevre hesaplama
+                double alan1 = en * boy;
+                double cevre1 = 2 * (en + boy);
+
+                // Sonuçları ekrana yazdırma
+                System.out.println("Dikdörtgenin eni: " + en);
+                System.out.println("Dikdörtgenin boyu: " + boy);
+                System.out.println("Dikdörtgenin alanı: " + alan1);
+                System.out.println("Dikdörtgenin çevresi: " + cevre1);
