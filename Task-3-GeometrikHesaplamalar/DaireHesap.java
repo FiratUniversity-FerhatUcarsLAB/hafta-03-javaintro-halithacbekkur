a0ac9bcbbe47975ea6bcfbb3ebefe6d013bef47c
halithacbekkur <mfadlatjamyh@gmail.com>
2025-10-18 21:07:32 +0300

    Add calculations for circle area and circumference

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,9 @@
+    System.out.println("_______________________________________");
+        double r = 5.5;
+        double pi = 3.14159;
+        double alan = pi * r * r ;
+        double cevre = 2 * pi * r ;
+
+         System . out . println ( " Yaricap : " + r ) ;
+         System . out . println ( " Alan : " + alan ) ;
+         System . out . println ( " Cevre : " + cevre ) ;
